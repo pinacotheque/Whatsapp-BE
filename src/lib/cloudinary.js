ed1a3dea64d11761ac97bfcc2dab9ddceed5ef83
Rabz92 <81884280+RabzTims-04@users.noreply.github.com>
2021-09-07 16:02:18 +0200

    messages and room cruds done

--- src/lib/cloudinary.js
+++ src/lib/cloudinary.js
@@ -5,10 +5,43 @@ import multer from "multer"
 const cloudinaryStorage = new CloudinaryStorage({
     cloudinary,
     params:{
-        folder:"Whatsapp"
+        folder:"Whatsapp/avatar"
+    }
+})
+
+const cloudinaryStoragePost = new CloudinaryStorage({
+    cloudinary,
+    params:{
+        folder:"Whatsapp/post"
+    }
+})
+
+const cloudinaryStorageBackground = new CloudinaryStorage({
+    cloudinary,
+    params:{
+        folder:"Whatsapp/background"
+    }
+})
+
+const cloudinaryStorageRoomAvatar = new CloudinaryStorage({
+    cloudinary,
+    params:{
+        folder:"Whatsapp/roomAvatar"
     }
 })
 
 export const uploadOnCloudinary = multer({
     storage: cloudinaryStorage
 }).single("avatar")
+
+export const uploadOnCloudinaryPost = multer({
+    storage: cloudinaryStoragePost
+}).single("post")
+
+export const uploadOnCloudinaryRoomBackground = multer({
+    storage: cloudinaryStorageBackground
+}).single("background")
+
+export const uploadOnCloudinaryRoomAvatar = multer({
+    storage: cloudinaryStorageRoomAvatar
+}).single("roomAvatar")
